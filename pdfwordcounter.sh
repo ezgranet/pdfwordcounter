afc861e04c2578669e12f9d26a4c8efd8d320cc6
ezgranet <ezgranet@gmail.com>
2019-06-21 23:05:06 -0700

    Switched out popplerbecause I was too stupid to solve it o therwise
    Yay

--- pdfwordcounter.sh
+++ pdfwordcounter.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-python copy_dylibs.py
 if [ $# -eq 0 ]
   then
     echo "Either drag a PDF to the window, or click File-> Open"
 else
-		popplar/0730/ban/pdftotaxt "$1" - | wc -w 
+		export PATH="/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin"
+	/Users/ezgranet/Documents/GitHub/pdfwordcounter/pdfwordcounter/pdfwordcounter.app/Contents/Resources/xpdf-tools-mac-4.01.01/bin64/pdftotext "$1" - | wc -w 
 fi
